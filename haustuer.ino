a89b450a17cd404035b2fb44169814b3312ed4dc
pilo1337 <pilo1337@me.com>
2022-03-20 18:40:54 +0100

    added debounce for input, fixed dumb shit and cleaned up code

--- haustuer.ino
+++ haustuer.ino
@@ -11,6 +11,11 @@
 #include <ArduinoJson.h>
 #include "Arduino.h"
 
+/*
+ * Un-Comment for Serial Debug Output
+ */
+//#define SerialDebug
+
 extern "C"
 {
   #include "user_interface.h"
@@ -23,24 +28,35 @@ os_timer_t Timer1;
 int Counter = 0;
            
 bool TickOccured = true;
-
 bool doorbellTriggered = false;
 
+bool shouldSaveConfig = false;
+char msgToPublish[60];
+
+bool bufState;
+long lastDebounceTime = 0;
+long debounceDelay = 150;
+
+/*
+ * Placeholders for MQTT Variables, these are entered in WiFiManager Portal, no need to change it here.
+ */
 char MQTT_BROKER[40] = "x.x.x.x";
 char MQTT_PORT[6] = "1883";
 
+/*
+ * These are the topics for the doorbell
+ */
 const char*    statusTopic = "/home/haustuer/klingel/status";
 const char*    doorOpenerTopic = "/home/haustuer/klingel/dooropener";
 const char*    modeTopic = "/home/haustuer/klingel/mode";
 const char*    doorbellTopic ="/home/haustuer/klingel";
 
+/*
+ * Input declarations
+ */
 const int doorbellInput = D1;
 const int modePin = D3;
 const int dooropenerPin = D4;
-
-bool shouldSaveConfig = false;
-
-char msgToPublish[60];
 
 /*
  * Handler
@@ -57,12 +73,14 @@ PubSubClient client(espClient);
  * Callback for WifiManager Settings
  */
 void saveConfigCallback () {
+  #ifdef SerialDebug
   Serial.println("Should save config");
+  #endif
   shouldSaveConfig = true;
 }
 
 /*
- * Timer Callback for timed Interrupt
+ * Timer Callback for timed Interrupt every 30 seconds (Heartbeat)
  */
 void timerCallback(void *pArg)
 {
@@ -74,6 +92,7 @@ void timerCallback(void *pArg)
  * Callback function for MQTT Subscribe
  */
 void callback(char* topic, byte* payload, unsigned int length) {
+  #ifdef SerialDebug
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -82,42 +101,71 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
+  #endif
   if (strcmp(topic,modeTopic) == 0 && (char)payload[0] == '1')
   {
-    digitalWrite(modePin,HIGH);
+    digitalWrite(modePin,LOW);
+    #ifdef SerialDebug
     Serial.println("switch to HA");  
+    #endif
     EEPROM.write(0, 1);
     TickOccured = true;
     
   }
   else if (strcmp(topic,modeTopic) == 0 && (char)payload[0] == '0')
   {
-    digitalWrite(modePin,LOW);
+    digitalWrite(modePin,HIGH);
+    #ifdef SerialDebug
     Serial.println("Switch to doorbell");
+    #endif
     EEPROM.write(0, 0);
     TickOccured = true;
   
   }
   else if (strcmp(topic,doorOpenerTopic) == 0 && (char)payload[0] == '1')
   {
+    #ifdef SerialDebug
     Serial.println("door opener triggered");
+    #endif
     digitalWrite(BUILTIN_LED, LOW);
-    digitalWrite(dooropenerPin, HIGH);
-    delay(4000);
-    Serial.println("door opener end");
-    digitalWrite(BUILTIN_LED, HIGH);  
     digitalWrite(dooropenerPin, LOW);
+    delay(4000);
+    #ifdef SerialDebug
+    Serial.println("door opener end");
+    #endif
+    digitalWrite(BUILTIN_LED, HIGH);  
+    digitalWrite(dooropenerPin, HIGH);
   }
   EEPROM.commit();
 }
 
 /*
- * Interrupt function for detecting falling edge on Input pin
+ * Interrupt function for debounce on Input pin
  */
-void ICACHE_RAM_ATTR doorbellFallingEdge()
+void ICACHE_RAM_ATTR doorbellDebounce()
 {
-  doorbellTriggered = true;
+  #ifdef SerialDebug
+  Serial.println("Interrupt: entered");
+  #endif
+  
+  int reading = digitalRead(doorbellInput);
+
+  if(reading == bufState) return;
+  boolean debounce = false;
+
+  if((millis() - lastDebounceTime) <= debounceDelay)
+  {
+    debounce = true;
+  }
+  lastDebounceTime = millis();
+  if(debounce) return;  
+
+  bufState = reading;
+
+  if (reading==1) doorbellTriggered = true;
+  #ifdef SerialDebug
+  Serial.println("Interrupt: button: " + String(reading));
+  #endif
 }
 
 /*
@@ -135,46 +183,64 @@ void setup() {
     
   Serial.begin(115200);
 
+  #ifdef SerialDebug
+  Serial.println("DEBUG MODE");
   Serial.println("mounting FS...");
-
+  wifiManager.setDebugOutput(true);
+  #else
+  wifiManager.setDebugOutput(false);
+  #endif
+  
   if (SPIFFS.begin()) {
+    #ifdef SerialDebug
     Serial.println("mounted file system");
+    #endif
     if (SPIFFS.exists("/config.json"))
     {
+      #ifdef SerialDebug
       Serial.println("reading config file");
+      #endif
       File configFile = SPIFFS.open("/config.json", "r");
       if (configFile)
       {
+        #ifdef SerialDebug
         Serial.println("opened config file");
+        #endif
         size_t size = configFile.size();
         std::unique_ptr<char[]> buf(new char[size]);
         configFile.readBytes(buf.get(), size);
         
-      #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+        #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
         DynamicJsonDocument json(1024);
         auto deserializeError = deserializeJson(json, buf.get());
         serializeJson(json, Serial);
         if ( ! deserializeError )
         {
-      #else
+        #else
           DynamicJsonBuffer jsonBuffer;
           JsonObject& json = jsonBuffer.parseObject(buf.get());
           json.printTo(Serial);
           if (json.success())
           {
-      #endif
+        #endif
+            #ifdef SerialDebug
             Serial.println("\nparsed json");
+            #endif
             strcpy(MQTT_BROKER, json["mqtt_server"]);
             strcpy(MQTT_PORT, json["mqtt_port"]);
           } else
           {
+            #ifdef SerialDebug
             Serial.println("failed to load json config");
+            #endif
         }
         configFile.close();
       }
     }
   } else {
+    #ifdef SerialDebug
     Serial.println("failed to mount FS");
+    #endif
   }
 
   WiFiManagerParameter custom_mqtt_server("server", "mqtt server", MQTT_BROKER, 40);
@@ -184,50 +250,58 @@ void setup() {
 
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
-  
+
   wifiManager.autoConnect("Haustuer");
 
   strcpy(MQTT_BROKER, custom_mqtt_server.getValue());
   strcpy(MQTT_PORT, custom_mqtt_port.getValue());
 
-  //save the custom parameters to FS
   if (shouldSaveConfig)
   {
+    #ifdef SerialDebug
     Serial.println("saving config");
-  #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+    #endif
+    #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
     DynamicJsonDocument json(1024);
-  #else
+    #else
     DynamicJsonBuffer jsonBuffer;
     JsonObject& json = jsonBuffer.createObject();
-  #endif
+    #endif
     json["mqtt_server"] = MQTT_BROKER;
     json["mqtt_port"] = MQTT_PORT;
 
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile)
     {
+      #ifdef SerialDebug
       Serial.println("failed to open config file for writing");
+      #endif
     }
 
-  #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
+    #ifdef ARDUINOJSON_VERSION_MAJOR >= 6
     serializeJson(json, Serial);
     serializeJson(json, configFile);
-  #else
+    #else
     json.printTo(Serial);
     json.printTo(configFile);
-  #endif
+    #endif
     configFile.close();
   }
-
+  
+  #ifdef SerialDebug
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
+  #endif
   
   ArduinoOTA.setHostname("Haustuer");
   ArduinoOTA.begin();
 
+  #ifdef SerialDebug
   Serial.println("Connecting MQTT");
   Serial.print((char *)MQTT_BROKER);
+  Serial.print(":");
   Serial.println(atoi(MQTT_PORT));
+  #endif
   client.setServer((char *)MQTT_BROKER, atoi(MQTT_PORT));
   client.setCallback(callback);
   client.connect("HaustuerV2");
@@ -237,9 +311,10 @@ void setup() {
   os_timer_setfn(&Timer1, timerCallback, &Counter);
   os_timer_arm(&Timer1, 30000, true);
 
-  digitalWrite(modePin,EEPROM.read(0));
+  digitalWrite(dooropenerPin, HIGH);
+  digitalWrite(modePin,!EEPROM.read(0));
 
-  attachInterrupt(digitalPinToInterrupt(doorbellInput), doorbellFallingEdge, FALLING);
+  attachInterrupt(digitalPinToInterrupt(doorbellInput), doorbellDebounce, CHANGE);
 }
 
 /*
@@ -249,7 +324,9 @@ void loop()
 {
   if (!client.connected())
   {
+    #ifdef SerialDebug
     Serial.println("disconnected, retrying");
+    #endif
     client.connect("HaustuerV2");
     client.subscribe(modeTopic);
     client.subscribe(doorOpenerTopic);
@@ -257,7 +334,9 @@ void loop()
 
   if (TickOccured)
   {
+    #ifdef SerialDebug
     Serial.println("Heartbeat Time!");
+    #endif
     if (client.connected())
     {
       snprintf (msgToPublish, 60, "%d", EEPROM.read(0));
@@ -268,7 +347,9 @@ void loop()
 
   if (doorbellTriggered)
   {
+    #ifdef SerialDebug
     Serial.println("hat geklingelt");
+    #endif
     client.publish(doorbellTopic, "DingDong");
     delay(2000);
     client.publish(doorbellTopic, " ");    
